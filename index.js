fb016157554a2d1409677e814a905429abf4be50
Sean Bailey <sean@actualsize.com>
2021-03-28 22:24:47 -0400

    basically working

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8000;
 
 const { Client } = require("pg");
 
 const client = new Client({
   connectionString:
-    "postgres://nick:admin@localhost:5432/bernard-pgh-local",
+    process.env.DATABASE_URL || "postgres://sean@localhost:5432/bernard-local",
 });
 
-client.connect();
+client.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log("connected");
+  }
+});
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,33 +27,49 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/query", (req, res) => {
+// this whole query is immensely vulnerable to SQL injection
+// but it's fine
+app.get("/query", async (req, res) => {
   let url = req.url;
-  let where = '';
+  let where = "";
 
   // If the query is a name query, search the first name, last name fields
-  if(url.includes("?name")){
-  	let param = url.split("=");
-  	where += ' WHERE "First Name" LIKE ' + "'%" + param[1].toUpperCase() + "%'" + ' OR "Last Name" LIKE ' + "'%" + param[1].toUpperCase() + "%' ";
-  // If the query is an address query, search by street name
-  } else if(url.includes("?address")){
-  	let param = url.split("=");
-  	where += ' WHERE "Street Name" LIKE ' + "'%" + param[1].toUpperCase() + "%' ";
+  if (url.includes("?name")) {
+    let param = url.split("=");
+    const name = decodeURI(param[1].toUpperCase()).trim();
+    let firstName, lastName;
+    // if space, split and search first and last name
+    if (name.includes(" ")) {
+      firstName = name.split(" ")[0];
+      lastName = name.split(" ")[1];
+    }
+    where +=
+      ' WHERE "First Name" LIKE ' +
+      "'%" +
+      (firstName || name) +
+      "%'" +
+      (firstName ? ' AND "Last Name" LIKE ' : ' OR "Last Name" LIKE ') +
+      "'%" +
+      (lastName || name) +
+      "%' ";
+    // console.log(where);
+    // If the query is an address query, search by street name
+  } else if (url.includes("?address")) {
+    let param = url.split("=");
+    where +=
+      ' WHERE "Street Name" LIKE ' + "'%" + param[1].toUpperCase() + "%' ";
   }
 
   // Construct the query
-  let query = 'SELECT "Last Name", "First Name", "Gender", "Party Code", "Street Name", "City", "Zip", "ID Number" FROM ' + '"public"."CityOnly"' + where + "LIMIT 10;";
+  let query =
+    'SELECT "Last Name", "First Name", "Gender", "Party Code", "House Number" || "House Number Suffix" AS "House Number", "Street Name", "City", "Zip", "ID Number" FROM ' +
+    '"public"."CityOnly"' +
+    where +
+    "LIMIT 20;";
 
-  client.query(
-  	query,
-    (err, res) => {
-      for(row in res.rows){
-      	console.log(res.rows[row]);
-      }
-      // client.end();
-    }
-  );
-  res.json(res.rows);
+  const queryres = await client.query(query);
+
+  res.json(queryres.rows);
 });
 
 app.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@
  */
 
 window.addEventListener("DOMContentLoaded", function () {
+  let voters;
   document
     .querySelector(".Wrapper--loading")
     .classList.remove("Wrapper--loading");
@@ -14,7 +15,11 @@ window.addEventListener("DOMContentLoaded", function () {
       req.open("GET", `/query?name=${encodeURIComponent(nameQuery)}`, true);
       req.send();
       req.onload = function () {
-        console.log(req.response);
+        // console.log(req.response);
+        document.querySelector([
+          '[data-js="name-results"]',
+        ]).innerHTML = renderResults(req.response);
+        voters = JSON.parse(req.response);
       };
       //   document.querySelector(
       //     '[data-js="name-results"]'
@@ -29,34 +34,211 @@ window.addEventListener("DOMContentLoaded", function () {
       req.open("GET", `/query?address=${encodeURIComponent(nameQuery)}`, true);
       req.send();
       req.onload = function () {
-        console.log(typeof(req.response));
+        console.log(typeof req.response);
         console.log(req.response);
       };
-      req.onreadystatechange = function() {
+      req.onreadystatechange = function () {
         console.log("ORSTC");
-        console.log(req.responeText);
-          if (req.readyState == XMLHttpRequest.DONE) {
-              console.log(req.responseText);
-          }
-      }
+        console.log(req.responseText);
+        if (req.readyState == XMLHttpRequest.DONE) {
+          // console.log(req.response);
+          document.querySelector(
+            '[data-js="address-results"]'
+          ).innerHTML = renderResults(req.response);
+          voters = JSON.parse(req.response);
+        }
+      };
       // document.querySelector(
       //   '[data-js="address-results"]'
       // ).innerHTML = renderResults(req.response);
     });
+
+  document
+    .querySelector('[data-js="name-results"]')
+    .addEventListener("change", (e) => {
+      if (e.target.matches("input")) {
+        const id = e.target.dataset.id;
+        const voter = voters.find((v) => v["ID Number"] === id);
+        if (e.target.checked) {
+          voter.isSelected = true;
+        } else {
+          voter.isSelected = false;
+        }
+
+        document.querySelector(
+          '[data-js="address-results"]'
+        ).innerHTML = renderResults(
+          searchByAddress({ query: addressQuery, voters })
+        );
+      }
+    });
+  document
+    .querySelector('[data-js="address-results"]')
+    .addEventListener("change", (e) => {
+      if (e.target.matches("input")) {
+        const id = e.target.dataset.id;
+        const voter = voters.find((v) => v["ID Number"] === id);
+        if (e.target.checked) {
+          voter.isSelected = true;
+        } else {
+          voter.isSelected = false;
+        }
+
+        document.querySelector(
+          '[data-js="name-results"]'
+        ).innerHTML = renderResults(searchByName({ query: nameQuery, voters }));
+      }
+    });
+  document
+    .querySelector('[data-js="export-button"]')
+    .addEventListener("click", (e) => {
+      exportResults(voters);
+    });
 });
 
 function renderResults(results) {
-    console.log("RENDERING!");
-    console.log(results);
-    if (!results.length) return '';
-    
+  console.log("RENDERING!");
+  results = JSON.parse(results);
+  console.log(results);
+  if (!results.length) return "";
 
-    return `
+  return `
     <thead class="Results-header">
-        <tr><th class="Results-header_cell"></th>${Object.keys(results[0]).filter((value) => value !== 'isSelected').map((key) => `<th class="Results-header_cell">${key}</th>`).join('')}</tr>
+        <tr><th class="Results-header_cell"></th>${Object.keys(results[0])
+          .filter((value) => value !== "isSelected")
+          .map((key) => `<th class="Results-header_cell">${key}</th>`)
+          .join("")}</tr>
     </thead>
     <tbody>
-        ${results.map((result) => `<tr class="Results-row"><td class="Results-row_cell"><input type="checkbox" data-id="${result[VOTERID]}" ${result.isSelected ? 'checked' : ''}></td>${Object.keys(result).filter((value) => value !== 'isSelected').map((key) => `<td class="Results-row_cell">${result[key]}</td>`).join('')}</tr>`).join('')}
+        ${results
+          .map(
+            (result) =>
+              `<tr class="Results-row"><td class="Results-row_cell"><input type="checkbox" data-id="${
+                result["ID Number"]
+              }" ${result.isSelected ? "checked" : ""}></td>${Object.keys(
+                result
+              )
+                .filter((value) => value !== "isSelected")
+                .map(
+                  (key) => `<td class="Results-row_cell">${result[key]}</td>`
+                )
+                .join("")}</tr>`
+          )
+          .join("")}
     </tbody>
-    `
+    `;
+}
+
+function queryMatchesVoterField({ normalizedQuery, voter, field }) {
+  return normalizeString(voter[field]).indexOf(normalizedQuery) !== -1;
+}
+
+function searchByName({ query, voters }) {
+  if (query.length < 3) return [];
+
+  const fields = Object.keys(voters[0]);
+  const numVoters = voters.length;
+  const matches = [];
+
+  const normalizedQuery = normalizeString(query);
+
+  for (let i = 0; i < numVoters; i++) {
+    const voter = voters[i];
+
+    if (
+      queryMatchesVoterField({ normalizedQuery, voter, field: "First Name" }) ||
+      queryMatchesVoterField({ normalizedQuery, voter, field: "Last Name" })
+    ) {
+      matches.push(voter);
+    }
+  }
+
+  return matches;
+}
+
+function searchByAddress({ query, voters }) {
+  if (query.length < 3) return [];
+
+  const fields = Object.keys(voters[0]);
+  const numVoters = voters.length;
+  const matches = [];
+
+  const normalizedQuery = normalizeString(query);
+
+  for (let i = 0; i < numVoters; i++) {
+    const voter = voters[i];
+
+    if (
+      (queryMatchesVoterField({
+        normalizedQuery,
+        voter,
+        field: "House Number",
+      }),
+      queryMatchesVoterField({
+        normalizedQuery,
+        voter,
+        field: "Street Name",
+      }) ||
+        queryMatchesVoterField({ normalizedQuery, voter, field: "City" }) ||
+        queryMatchesVoterField({ normalizedQuery, voter, field: "Zip" }))
+    ) {
+      matches.push(voter);
+    }
+  }
+
+  return matches;
+}
+
+function exportResults(voters) {
+  const selections = voters
+    .filter((voter) => voter.isSelected)
+    .map((voter) => {
+      delete voter.isSelected;
+      return voter;
+    });
+  exportCSVFile(selections);
+}
+
+function convertToCSV(objArray) {
+  const array = typeof objArray !== "object" ? JSON.parse(objArray) : objArray;
+  let str = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let line = "";
+    for (let index in array[i]) {
+      if (line != "") line += ",";
+      line += array[i][index];
+    }
+
+    str += line + "\r\n";
+  }
+
+  return str;
+}
+
+function exportCSVFile(items) {
+  const jsonObject = JSON.stringify(items);
+
+  const csv = convertToCSV(jsonObject);
+
+  const exportedFilename = "voters.csv";
+
+  const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+  if (navigator.msSaveBlob) {
+    // IE 10+
+    navigator.msSaveBlob(blob, exportedFilename);
+  } else {
+    const link = document.createElement("a");
+    if (link.download !== undefined) {
+      // feature detection
+      // Browsers that support HTML5 download attribute
+      const url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", exportedFilename);
+      link.style.visibility = "hidden";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
 }

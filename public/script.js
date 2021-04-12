7fee44d53b2c9a29439a2b6923f44ae51a236cac
Jenna Zeigen <jenna.zeigen@gmail.com>
2021-04-11 22:14:41 -0400

    and yet it persisted

--- public/script.js
+++ public/script.js
@@ -1,9 +1,11 @@
 /**
  * The frontend script that handles querying the database and getting the results.
  */
+const saved = {};
+let searchResults = [];
+const idKey = "ID Number";
 
 window.addEventListener("DOMContentLoaded", function () {
-  let voters;
   document
     .querySelector(".Wrapper--loading")
     .classList.remove("Wrapper--loading");
@@ -13,101 +15,81 @@ window.addEventListener("DOMContentLoaded", function () {
     .addEventListener("keyup", (e) => {
       email = e.target.value;
       if (email.indexOf('@') > -1) {
-        document.querySelector(".SearchWrapper--hidden").classList.remove("SearchWrapper--hidden");
+        const hidden = document.querySelector(".SearchWrapper--hidden")
+        if (hidden) hidden.classList.remove("SearchWrapper--hidden");
       }
     });
   
   document
     .querySelector('[data-js="input-name"]')
     .addEventListener("keyup", (e) => {
-      nameQuery = e.target.value;
+      nameQuery = e.target.value.trim();
+
+      // clear results when it's an empty query
+      if (!nameQuery.length) {
+        document.querySelector([
+          '[data-js="name-results"]',
+        ]).innerHTML = '';
+        return;
+      }
+
       var req = new XMLHttpRequest();
       req.open("GET", `/query?name=${encodeURIComponent(nameQuery)}`, true);
       req.send();
       req.onload = function () {
-        // console.log(req.response);
+        searchResults = JSON.parse(req.response);
+        
+        for (let i = 0; i < searchResults.length; i++) {
+          if (saved[searchResults[i][idKey]]) {
+            searchResults[i].isSelected = true;
+          }
+        }
+
         document.querySelector([
           '[data-js="name-results"]',
-        ]).innerHTML = renderResults(req.response);
-        voters = JSON.parse(req.response);
+        ]).innerHTML = renderResults(searchResults);
       };
-      //   document.querySelector(
-      //     '[data-js="name-results"]'
-      //   ).innerHTML = renderResults(searchByName({ query: nameQuery, voters }));
     });
-  // document
-  //   .querySelector('[data-js="input-address"]')
-  //   .addEventListener("keyup", (e) => {
-  //     addressQuery = e.target.value;
-  //     nameQuery = e.target.value;
-  //     var req = new XMLHttpRequest();
-  //     req.open("GET", `/query?address=${encodeURIComponent(nameQuery)}`, true);
-  //     req.send();
-  //     req.onload = function () {
-  //       console.log(typeof req.response);
-  //       console.log(req.response);
-  //     };
-  //     req.onreadystatechange = function () {
-  //       console.log("ORSTC");
-  //       console.log(req.responseText);
-  //       if (req.readyState == XMLHttpRequest.DONE) {
-  //         // console.log(req.response);
-  //         document.querySelector(
-  //           '[data-js="address-results"]'
-  //         ).innerHTML = renderResults(req.response);
-  //         voters = JSON.parse(req.response);
-  //       }
-  //     };
-  //     // document.querySelector(
-  //     //   '[data-js="address-results"]'
-  //     // ).innerHTML = renderResults(req.response);
-  //   });
+
+  function onCheck(e) {
+    if (e.target.matches("input")) {
+      const id = e.target.dataset.id;
+      const voter = searchResults.find((v) => v["ID Number"] === id);
+      voter.isSelected = true;
+      if (e.target.checked) {
+        saved[id] = voter;
+        voter.isSelected = true;
+      } else {
+        delete saved[id];
+        voter.isSelected = false;
+      }
+    }
+
+    document.querySelector([
+      '[data-js="saved-voters"]',
+    ]).innerHTML = renderResults(Object.values(saved));
+
+    document.querySelector([
+      '[data-js="name-results"]',
+    ]).innerHTML = renderResults(searchResults);
+  }
 
   document
     .querySelector('[data-js="name-results"]')
-    .addEventListener("change", (e) => {
-      if (e.target.matches("input")) {
-        const id = e.target.dataset.id;
-        const voter = voters.find((v) => v["ID Number"] === id);
-        if (e.target.checked) {
-          voter.isSelected = true;
-        } else {
-          voter.isSelected = false;
-        }
+    .addEventListener("change", onCheck);
 
-        // document.querySelector(
-        //   '[data-js="address-results"]'
-        // ).innerHTML = renderResults(
-        //   searchByAddress({ query: addressQuery, voters })
-        // );
-      }
-    });
-  // document
-  //   .querySelector('[data-js="address-results"]')
-  //   .addEventListener("change", (e) => {
-  //     if (e.target.matches("input")) {
-  //       const id = e.target.dataset.id;
-  //       const voter = voters.find((v) => v["ID Number"] === id);
-  //       if (e.target.checked) {
-  //         voter.isSelected = true;
-  //       } else {
-  //         voter.isSelected = false;
-  //       }
+  document
+    .querySelector('[data-js="saved-voters"]')
+    .addEventListener("change", onCheck);
 
-  //       document.querySelector(
-  //         '[data-js="name-results"]'
-  //       ).innerHTML = renderResults(searchByName({ query: nameQuery, voters }));
-  //     }
-  //   });
   document
     .querySelector('[data-js="export-button"]')
     .addEventListener("click", (e) => {
-      exportResults(voters);
+      exportResults();
     });
 });
 
 function renderResults(results) {
-  results = JSON.parse(results);
   if (!results.length) return "";
 
   const fieldsToDisplay = ['Last Name', 'First Name', 'Street Name', 'City', 'Zip']
@@ -124,7 +106,7 @@ function renderResults(results) {
           .map(
             (result) =>
               `<tr class="Results-row"><td class="Results-row_cell"><input type="checkbox" data-id="${
-                result["ID Number"]
+                result[idKey]
               }" ${result.isSelected ? "checked" : ""}></td>${Object.keys(
                 result
               )
@@ -143,70 +125,8 @@ function queryMatchesVoterField({ normalizedQuery, voter, field }) {
   return normalizeString(voter[field]).indexOf(normalizedQuery) !== -1;
 }
 
-function searchByName({ query, voters }) {
-  if (query.length < 3) return [];
-
-  const fields = Object.keys(voters[0]);
-  const numVoters = voters.length;
-  const matches = [];
-
-  const normalizedQuery = normalizeString(query);
-
-  for (let i = 0; i < numVoters; i++) {
-    const voter = voters[i];
-
-    if (
-      queryMatchesVoterField({ normalizedQuery, voter, field: "First Name" }) ||
-      queryMatchesVoterField({ normalizedQuery, voter, field: "Last Name" })
-    ) {
-      matches.push(voter);
-    }
-  }
-
-  return matches;
-}
-
-function searchByAddress({ query, voters }) {
-  if (query.length < 3) return [];
-
-  const fields = Object.keys(voters[0]);
-  const numVoters = voters.length;
-  const matches = [];
-
-  const normalizedQuery = normalizeString(query);
-
-  for (let i = 0; i < numVoters; i++) {
-    const voter = voters[i];
-
-    if (
-      (queryMatchesVoterField({
-        normalizedQuery,
-        voter,
-        field: "House Number",
-      }),
-      queryMatchesVoterField({
-        normalizedQuery,
-        voter,
-        field: "Street Name",
-      }) ||
-        queryMatchesVoterField({ normalizedQuery, voter, field: "City" }) ||
-        queryMatchesVoterField({ normalizedQuery, voter, field: "Zip" }))
-    ) {
-      matches.push(voter);
-    }
-  }
-
-  return matches;
-}
-
-function exportResults(voters) {
-  console.log('jenna click')
-  const selections = (voters || [])
-    .filter((voter) => voter.isSelected)
-    .map((voter) => {
-      delete voter.isSelected;
-      return voter;
-    });
+function exportResults() {
+  const selections = Object.values(saved).map((savee) => delete savee.isSelected);
   exportCSVFile(selections);
 }
 
@@ -233,39 +153,16 @@ function convertToCSV(objArray) {
 function exportCSVFile(items) {
   const jsonObject = JSON.stringify(items);
   const csv = convertToCSV(jsonObject);
-  // const exportedFilename = "voters.csv";
-  // const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
 
   // Create the request to the server to send the CSV
   var formData = new FormData();
   formData.append("file", csv);
-  // formData.append("personName", "NAMExxx");
 
   var req = new XMLHttpRequest();
-  console.log('jenna sending request')
   req.open("POST", `/api/mail`, true);
   req.send(formData);
 
   req.onload = function () {
     window.location = '/thanks';
   };
-
-
-  // if (navigator.msSaveBlob) {
-  //   // IE 10+
-  //   navigator.msSaveBlob(blob, exportedFilename);
-  // } else {
-  //   const link = document.createElement("a");
-  //   if (link.download !== undefined) {
-  //     // feature detection
-  //     // Browsers that support HTML5 download attribute
-  //     const url = URL.createObjectURL(blob);
-  //     link.setAttribute("href", url);
-  //     link.setAttribute("download", exportedFilename);
-  //     link.style.visibility = "hidden";
-  //     document.body.appendChild(link);
-  //     link.click();
-  //     document.body.removeChild(link);
-  //   }
-  // }
 }
